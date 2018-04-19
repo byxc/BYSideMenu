77ddf22ed1728c6448339821a685e44721411ca0
袁诚 <yuancheng@a8sport.com>
2018-04-19 11:06:28 +0800

    更新注释

--- BYSideMenuDemo/BYSideMenu/SideMenuViewController.h
+++ BYSideMenuDemo/BYSideMenu/SideMenuViewController.h
@@ -3,7 +3,7 @@
 //  TestSideMenu
 //
 //  Created by 白云 on 2017/4/26.
-//  Copyright © 2017年 深圳嘉奇科技有限公司. All rights reserved.
+//  Copyright © 2017年 白云. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
